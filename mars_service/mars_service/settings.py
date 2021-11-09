c40ee2bf8a79cc2a19d5dfd02bc130253b2d309f
chipolina <suddenig@gmail.com>
2021-11-09 21:32:46 +0300

    Настроил подключение к ДБ

--- mars_service/mars_service/settings.py
+++ mars_service/mars_service/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'mars_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mars_db',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
